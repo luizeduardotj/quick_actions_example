a0d4ded811122b10984368d9a1c13ebc9db375c3
Luiz Eduardo <luiz.oliveira@blubybs2.com>
2020-11-28 21:00:42 -0300

    git init

---
+++ lib/main.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:quick_actions/quick_actions.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String shortcut = "Sem nenhuma ação";
+
+  @override
+  void initState() {
+    super.initState();
+    final QuickActions quickActions = QuickActions();
+
+    quickActions.initialize((String shortcutType) {
+      setState(() {
+        if (shortcutType != null) shortcut = shortcutType;
+      });
+    });
+
+    quickActions.setShortcutItems(
+      <ShortcutItem>[
+        // NOTE: This first action icon will only work on iOS.
+        // In a real world project keep the same file name for both platforms.
+        const ShortcutItem(
+          type: 'action_one',
+          localizedTitle: 'Action one',
+          icon: 'AppIcon',
+        ),
+        // NOTE: This second action icon will only work on Android.
+        // In a real world project keep the same file name for both platforms.
+        const ShortcutItem(
+            type: 'action_two',
+            localizedTitle: 'Action two',
+            icon: 'ic_launcher'),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: shortcut),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
